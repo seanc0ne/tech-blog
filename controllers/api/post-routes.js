cdad8360e8836b1759c7dd81c0cbb366193988d9
Sean Cone <seandoesitbetter@gmail.com>
2020-08-22 14:35:59 -0700

    Update post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -33,3 +33,35 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
           });
       });
+
+      router.get('/:id', (req, res) => {
+        Post.findOne({
+          where: {
+            id: req.params.id
+          },
+          attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          ],
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        })
+          .then(dbPostData => {
+            if (!dbPostData) {
+              res.status(404).json({ message: 'POST NOT FOUND' });
+              return;
+            }
+            res.json(dbPostData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
