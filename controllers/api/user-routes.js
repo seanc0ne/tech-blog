074e0bbf7dc771f65fbb40f2fd537ccf43c389e4
Sean Cone <seandoesitbetter@gmail.com>
2020-08-21 17:49:08 -0700

    Update user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,22 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
