bbd1ded71a8b2b6800bc1a0e5b998f6521e1f69f
Sean Cone <seandoesitbetter@gmail.com>
2020-08-22 10:56:38 -0700

    Update post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,2 +1,15 @@
 const router = require('express').Router();
 const { Post, User, Vote, Comment } = require("../../models");
+const sequelize = require('../../config/connection');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      order: [['created_at', 'DESC']],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      
