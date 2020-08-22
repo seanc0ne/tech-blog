bcc59235a83450bddaee25395d4c9d0ee1edd6a7
Sean Cone <seandoesitbetter@gmail.com>
2020-08-21 17:57:59 -0700

    Updating user-routes.js, slightly changing schema

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -20,3 +20,31 @@ router.post("/", (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post("/login", (req,res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'USER ACCOUNT NOT FOUND' });
+            return;
+        }
+
+        const workingPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!workingPassword) {
+            res.status(400).json({ message: 'PASSWORD WRONG' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'SHE LOGGED IN HENNY' })
+        });
+    });
+});
