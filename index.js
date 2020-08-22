cd773939cd048f6816e844fdd74dfe8a8cfd4192
Sean Cone <seandoesitbetter@gmail.com>
2020-08-21 19:32:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ const hbs = exphbs.create({
     }
   }
 });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require('./controllers/'));
  
 // configure express
 const app = express();
