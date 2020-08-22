3752a5c9ef0b311f8ddba0bb389feb003253b42b
Sean Cone <seandoesitbetter@gmail.com>
2020-08-21 18:02:43 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize('tech_blog_db', 'admin', 'password', {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3001
     });
 
 module.exports = sequelize;
