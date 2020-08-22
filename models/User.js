e4bcba69031293a7f3ec1907af55a73b4374fe8e
Sean Cone <seandoesitbetter@gmail.com>
2020-08-21 17:18:49 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -31,7 +31,13 @@ User.init(
     {
         hooks: {
             async beforeCreate(newUserData) {
-                
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
             }
         }
     }
