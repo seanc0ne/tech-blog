0bb745abe45232b2964f2013fbfa96699308daa5
Sean Cone <seandoesitbetter@gmail.com>
2020-08-22 12:07:16 -0700

    Update post-routes.js

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -12,4 +12,24 @@ router.get('/', (req, res) => {
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
-      
+      include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+       })
+          .then(dbPostData => res.json(dbPostData))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
