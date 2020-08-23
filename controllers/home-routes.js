6b888357783296aca2e09ec03b984a2bede4d4f5
Sean Cone <seandoesitbetter@gmail.com>
2020-08-23 00:13:29 -0700

    Exhausted after midnight, listening to Cyndi Lauper and coding home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,3 +2,6 @@
 // api for retrieving posts (get all posts)
 // new post API for a comment
 // anon user view 
+
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
