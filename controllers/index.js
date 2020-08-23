e255d80b91e8536b435f6403b6381c65c304abef
Sean Cone <seandoesitbetter@gmail.com>
2020-08-23 00:39:35 -0700

    Coding logout in user-routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homepage-routes.js');
+const dashboardRoutes = require('./user-dashboard-routes.js');
 
-// const apiRoutes = require('./api');
-
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
