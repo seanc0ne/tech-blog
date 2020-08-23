cdc359fb6e6fedd6e6cdc654b0107f81c21d3146
Sean Cone <seandoesitbetter@gmail.com>
2020-08-23 00:03:03 -0700

    Update dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,52 @@
 // creation
 // editing capabilities
 // delete
+
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const userAuth = require('../utils/auth');
+
+router.get('/', userAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+              model: Comment,
+              attributes: [
+                'id', 
+                'comment_text', 
+                'post_id', 
+                'user_id', 
+                'created_at'],
+              include: {
+                  model: User,
+                  attributes: ['username']
+              }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(Post => Post.get({ plain: true}));
+            res.render('dashboard', {
+                posts,
+                loggedIn: true
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
