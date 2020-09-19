03ee5711133146c93297b3fe965a3dbe0e9895d4
Sean Cone <seandoesitbetter@gmail.com>
2020-09-18 22:05:37 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,23 +2,38 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongsTo(User, {
-    foreignKey: 'userId',
+User.hasMany(Post, {
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-Post.hasMany(Comment, {
-    foreignKey: 'postId',
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'userId',
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
     onDelete: 'CASCADE'
 });
 
 module.exports = {
     User,
-    Comment,
-    Post
+    Post,
+    Comment
 };
